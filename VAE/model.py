5e38ae9fdda63c639437290861f3aa1e0650b559
Shivendra <shivendra.singh@in.bosch.com>
2024-09-18 18:03:41 +0530

    added the VAE part and running model

--- VAE/model.py
+++ VAE/model.py
@@ -4,6 +4,9 @@ from torch.utils.data import DataLoader
 from dataloader import custom_dataset
 import numpy as np
 import torch
+import matplotlib.pyplot as plt
+from PIL import Image
+
 class vae_arch(nn.Module):
     ## Input - 28 *28 image with values 0-255 
 
@@ -22,12 +25,28 @@ class vae_arch(nn.Module):
                     nn.Linear(256,784),
                     nn.Sigmoid()
                  )
+        self.mu_layer = nn.Linear(self.z_layer, self.z_layer)
+        self.log_var_layer = nn.Linear(self.z_layer, self.z_layer)
+    
 
     def forward(self,inp_img):
+        ## From encoder , we get the mu and sigma vectors depending on the size of Z vector
         z = self.encoder_layer(inp_img)
-        recon_img = self.decoder_layer(z)
-        return z,recon_img
+        mu = self.mu_layer(z)
+        log_var = self.log_var_layer(z)
+        std = torch.exp(0.5 * log_var)
+        ## random value vector from gaussian distribution
+        sample_z = torch.randn_like(std)
+        
+        ## Obtain a Zi  = mu_i + sigma_i * (Normal distribution(0,1)) 
+        encoded = mu + std * sample_z
+        recon_img = self.decoder_layer(encoded)
+        return encoded,recon_img
 
+    def sample(self):
+        z = torch.randn(1,self.z_layer)
+        recon = self.decoder_layer(z)
+        return recon
 
 
 if __name__ == "__main__" : 
@@ -36,12 +55,19 @@ if __name__ == "__main__" :
     sample_dataloader = DataLoader(dataset_obj,batch_size =4)
     sample = next(iter(sample_dataloader))
     label,image = sample[:,0],sample[:,1:]
-    image = image.type(torch.uint8)
-    print(image.shape,image.dtype)
+    ## transformation
+    image = image/255
     vae_obj = vae_arch()
     z,target_img = vae_obj.forward(image)
-    print(target_img)
 
+    ## sampling from the dataset
+    gen_image = vae_obj.sample()
+    gen_image = torch.round(gen_image *255)
+    gen_image = gen_image.detach().numpy().astype(np.uint8)
+    gen_image = gen_image.reshape((28,28))
+    buf = Image.fromarray(gen_image)
+    buf.save("generated_sample.png")
+    print(gen_image)
     
 
 
